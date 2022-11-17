1eac27e3c97540765a4453e0a48027c6eef26edf
SeungJoonChoi <tmdwns0845@naver.com>
2022-11-17 20:54:58 +0900

    221117

--- cpp04/ex01/main.cpp
+++ cpp04/ex01/main.cpp
@@ -3,26 +3,31 @@
 
 int main()
 {
-    Cat cat1;
-    Cat cat2;
+    {
+        
+    }
+    {
+        Cat cat1;
+        Cat cat2;
 
-    cat1.getBrain()->setIdeas("Cat");
-    cat2 = cat1;
+        cat1.getBrain()->setIdeas("Cat");
+        cat2 = cat1;
 
-    std::cout << std::endl;
-    std::cout << "Cat1 Brain" << std::endl;
-    cat1.getBrain()->printIdeas();
-    std::cout << "Cat2 Brain" << std::endl;
-    cat2.getBrain()->printIdeas();
-    std::cout << std::endl;
+        std::cout << std::endl;
+        std::cout << "Cat1 Brain" << std::endl;
+        cat1.getBrain()->printIdeas();
+        std::cout << "Cat2 Brain" << std::endl;
+        cat2.getBrain()->printIdeas();
+        std::cout << std::endl;
 
-    cat1.getBrain()->setIdeas("?");
+        cat1.getBrain()->setIdeas("?");
 
-    std::cout << "Cat1 Brain" << std::endl;
-    cat1.getBrain()->printIdeas();
-    std::cout << "Cat2 Brain" << std::endl;
-    cat2.getBrain()->printIdeas();
-    std::cout << std::endl;
+        std::cout << "Cat1 Brain" << std::endl;
+        cat1.getBrain()->printIdeas();
+        std::cout << "Cat2 Brain" << std::endl;
+        cat2.getBrain()->printIdeas();
+        std::cout << std::endl;
+    }
 
     return 0;
 }
