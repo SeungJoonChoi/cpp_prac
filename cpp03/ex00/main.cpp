f2e5b3e8e07883c0d1557556d242dd454b767f19
SeungJoonChoi <tmdwns0845@naver.com>
2022-11-18 12:59:13 +0900

    221118

--- cpp03/ex00/main.cpp
+++ cpp03/ex00/main.cpp
@@ -9,5 +9,11 @@ int main()
     enemy.takeDamage(me.getAttackDamage());
     enemy.beRepaired(5);
 
+    for(int i = 0; i < 6; ++i)
+        enemy.takeDamage(3);
+    enemy.beRepaired(5);
+    enemy.attack(me.getName());
+
+
     return 0;
 }
