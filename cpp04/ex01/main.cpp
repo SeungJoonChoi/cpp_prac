42c8571ef6621e29e58878eb8ccffea3f037ac72
SeungJoonChoi <tmdwns0845@naver.com>
2022-11-18 18:57:33 +0900

    221118

--- cpp04/ex01/main.cpp
+++ cpp04/ex01/main.cpp
@@ -4,9 +4,22 @@
 int main()
 {
     {
-        
+        Animal *meta[6];
+        for(int i = 0; i < 6; ++i)
+        {
+            if(i < 3)
+                meta[i] = new Cat;
+            else
+                meta[i] = new Dog;
+        }
+        for(int i = 0; i < 6; ++i)
+            meta[i]->makeSound();
+
+        for(int i = 0; i < 6; ++i)
+            delete meta[i];
     }
     {
+        std::cout << "@@@@@Deep Copy Test@@@@@" << std::endl;
         Cat cat1;
         Cat cat2;
 
