bf1abf48fa98f6e53b6ffd693046a3a99c7873e5
SeungJoonChoi <tmdwns0845@naver.com>
2022-12-06 21:07:37 +0900

    221206

--- cpp08/ex02/MutantStack.hpp
+++ cpp08/ex02/MutantStack.hpp
@@ -26,7 +26,7 @@ public:
         return *this;
     }
 
-    typedef typename std::stack<T>:: container_type::iterator iterator;
+    typedef typename std::stack<T>::container_type::iterator iterator;
     iterator begin()
     {
         return this->c.begin();
@@ -37,7 +37,7 @@ public:
         return this->c.end();
     }
 
-    typedef typename std::stack<T>:: container_type::const_iterator const_iterator;
+    typedef typename std::stack<T>::container_type::const_iterator const_iterator;
     const_iterator cbegin()
     {
         return this->c.cbegin();
@@ -48,7 +48,7 @@ public:
         return this->c.cend();
     }
 
-    typedef typename std::stack<T>:: container_type::reverse_iterator reverse_iterator;
+    typedef typename std::stack<T>::container_type::reverse_iterator reverse_iterator;
     reverse_iterator rbegin()
     {
         return this->c.rbegin();
@@ -59,7 +59,7 @@ public:
         return this->c.rend();
     }
 
-    typedef typename std::stack<T>:: container_type::const_reverse_iterator const_reverse_iterator;
+    typedef typename std::stack<T>::container_type::const_reverse_iterator const_reverse_iterator;
     const_reverse_iterator crbegin()
     {
         return this->c.crbegin();
